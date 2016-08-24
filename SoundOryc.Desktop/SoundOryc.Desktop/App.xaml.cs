39d15066d0a8cead9b761f8af0c4f796dcb5cec2
Alejandro Madrid <corpex@gmx.com>
2016-08-24 16:31:04 +0200

    Added Mahapss Metro integration & first style changes

---
+++ SoundOryc.Desktop/SoundOryc.Desktop/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MahApps.Metro;
+using System;
+using System.Windows;
+
+namespace SoundOryc.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        { 
+            // add custom accent and theme resource dictionaries
+            ThemeManager.AddAccent("CustomAccent1", new Uri("pack://application:,,,/SoundOryc.Desktop;component/Views/CustomAccent.xaml"));
+
+            // get the theme from the current application
+            AppTheme theme = ThemeManager.DetectAppStyle(Application.Current).Item1;
+
+            // now use the custom accent
+            ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("CustomAccent1"), theme);
+
+            base.OnStartup(e);
+        }
+    }
+}
